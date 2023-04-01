3ea8cf2a8c08ae831cb92e6f204591a905562d8f
jules <jules.ferlin@gmail.com>
2023-04-01 17:03:47 +0200

    added test file

---
+++ test.php
@@ -0,0 +1,40 @@
+<?php
+include ('./PageParts/databaseFunctions.php');
+$configs = include('config.php');
+ConnectDatabase();
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Test</title>
+    </head>
+    <body>
+        <h1><?php echo $configs['host']?></h1>
+        <h1><?php echo $configs['username']?></h1>
+        <h1><?php echo $configs['password']?></h1>
+        <h1><?php echo $configs['dbname']?></h1>
+        <p>Test</p>
+        <?php
+
+        $query =
+            "SELECT * FROM `utilisateurs`";
+        $result = $conn->query($query);
+
+        if (mysqli_num_rows($result) != 0) {
+            echo "<ul>";
+            while ($row = $result->fetch_assoc()) {
+                echo '<li>Découvrir <a href="./showBlog.php?userID=' . $row["id"] . '">le blog de ' . $row["pseudo"] . '</a></li>';
+            }
+            echo "</ul>";
+        } else {
+            echo '<p class="warning"> Aucun utilisateur/blog n\'existe dans le système pour l\'instant!</p>';
+        }
+
+
+        ?>
+    </body>
+</html>
+
