ca9f95f20e0149bdd58cae93ad5eaa96179a71e7
jules <jules.ferlin@gmail.com>
2023-04-08 19:40:55 +0200

    Ajout de variables

--- PageParts/variables.php
+++ PageParts/variables.php
@@ -5,7 +5,9 @@ $imagePathLink = "http://chris.ferlin.fr:90/Projet/data/users/images/";
 $imagePath = "./data/users/images/";
 $littleImagePathLink = "http://chris.ferlin.fr:90/Projet/images/";
 $rulesGamesPathLink = "http://chris.ferlin.fr:90/Projet/data/games/rules/";
+$rulesGamesPath = "./data/games/rules/";
 $imagesGamesPathLink = "http://chris.ferlin.fr:90/Projet/data/games/images/";
+$imagesGamesPath = "./data/games/images/";
 $PagenewAccount = false;
 ?>
 
