1fd9c55f93a01585f6a0e68fbfe009a8ab86a182
Pierre Schneiderlin <pierre.schneiderlin@utbm.fr>
2023-04-11 22:56:13 +0200

    Amélioration de l'organisation du stockage des liens dans les variables

--- PageParts/variables.php
+++ PageParts/variables.php
@@ -1,11 +1,12 @@
 <?php
 $nomsite = "Dice & Pixels";
-$imagePathLink = "http://chris.ferlin.fr:90/Projet/data/users/images/";
+$headLink = "http://chris.ferlin.fr:90/Projet/";
+$imagePathLink = $headLink."data/users/images/";
 $imagePath = "./data/users/images/";
-$littleImagePathLink = "http://chris.ferlin.fr:90/Projet/images/";
-$rulesGamesPathLink = "http://chris.ferlin.fr:90/Projet/data/games/rules/";
+$littleImagePathLink = $headLink."images/";
+$rulesGamesPathLink = $headLink."data/games/rules/";
 $rulesGamesPath = "./data/games/rules/";
-$imagesGamesPathLink = "http://chris.ferlin.fr:90/Projet/data/games/images/";
+$imagesGamesPathLink = $headLink."data/games/images/";
 $imagesGamesPath = "./data/games/images/";
 $PagenewAccount = false;
 ?>
