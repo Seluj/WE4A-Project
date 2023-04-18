f49316d7dc949b55c75f3b2ea95e914a2b24b588
jules <jules.ferlin@gmail.com>
2023-04-18 20:10:46 +0200

    modification des valeurs de variables dans variables.php

--- PageParts/variables.php
+++ PageParts/variables.php
@@ -2,12 +2,12 @@
 $nomSite = "Dice & Pixels";
 $nomSectionJeux = "Jeux proposés";
 $headLink = "http://chris.ferlin.fr:6969/Projet/";
-$imagePathLink = $headLink."data/users/images/";
+$imagePathLink = $headLink."data/users/images";
 $imagePath = "./data/users/images/";
 $littleImagePathLink = $headLink."images/";
-$rulesGamesPathLink = $headLink."data/games/rules/";
+$rulesGamesPathLink = $headLink."data/games/rules";
 $rulesGamesPath = "./data/games/rules/";
-$imagesGamesPathLink = $headLink."data/games/images/";
+$imagesGamesPathLink = $headLink."data/games/images";
 $imagesGamesPath = "./data/games/images/";
 $pageNewAccount = false;
 ?>
