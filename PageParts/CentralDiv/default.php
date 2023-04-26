6d18485c5b40c31b17c378c38657f1f899e9e115
Pierre Schneiderlin <pierre.schneiderlin@utbm.fr>
2023-04-26 20:18:10 +0200

    Déplacement du bouton d'ajout/modification d'un jeu, petites moficiation du texte et ajout d'une liste de topics

--- PageParts/CentralDiv/default.php
+++ PageParts/CentralDiv/default.php
@@ -1,5 +1,5 @@
-<div class="deroulant">
-    <img id="image_titre" src="./images/fond_titre.jpg" alt="Bienvenue">
+<div id="default_div">
+    <img id="image_titre" src="./images/fond_titre1.png" alt="Bienvenue">
     <?php if(!$connecte){ ?>
         <p class="texte_accueil">Venez vous plongez avec nous dans le monde des jeux de société et des jeux vidéos ! Vous souhaitez découvrir de
             nouveaux jeux, discuter stratégie et partager vos expériences de jeu avec d'autres passionnés dans un monde de
@@ -9,16 +9,13 @@
             Vous avez la possibilité de créer des topics à propos de jeux, d'y échanger avec les autres utilisateurs
             au travers de messages, et de participer à des topics créés par d'autres. </p>
     <?php }else{ ?>
-        <p class="texte_accueil"><?php echo "Bienvenue " . $utilisateur . " !\n" ?>Découvre ici de nouveaux jeux ! Quelques-uns te sont proposés
+        <p class="texte_accueil">Bienvenue <b><?php echo $utilisateur ?></b> ! <br><br>Découvre ici de nouveaux jeux ! Quelques-uns te sont proposés
             dans la section "<?php echo $nomSectionJeux ?>", mais si tu en cherches un en particulier, utilise la barre de recherche dans le
             bandeau supérieur.<br>Tu peux également y choisir le type de jeux qui te sont proposés !<br>Si tu as une question ou souhaites discuter
             d'un aspect d'un jeu avec d'autres personnes, il te suffit d'aller sur la page du jeu et de créer un topic sur le sujet, et tout le monde
             pourra venir y participer !</p>
 
     <?php }
-    if ($administrateur) {?>
-        <div id="BoutonModifierJeu" class="linkBox">
-            <a class="police" href="./newGame.php">Ajouter Jeu</a>
-        </div>
-    <?php } ?>
+    include("./PageParts/listTopics.php")?>
+
 </div>
