ccf47ef2cf72298a052ad713e6c301b547d9a22a
Pierre Schneiderlin <pierre.schneiderlin@utbm.fr>
2023-04-26 11:02:33 +0200

    Création d'un fichier head.php contenant le corps de la balise head

---
+++ PageParts/head.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<title><?php echo $nomSite ?></title>
+<link rel="stylesheet" href="./Styles/style.css">
+<link rel="stylesheet" href="./Styles/header.css">
+<link rel="stylesheet" href="./Styles/interaction.css">
+<link rel="stylesheet" href="./Styles/container.css">
+<link rel="icon" href="images/icone.png">
