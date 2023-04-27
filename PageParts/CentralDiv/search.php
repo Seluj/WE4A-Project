32ba1bc92370ecdbfaef92873fcb2cce4619d497
Pierre Schneiderlin <pierre.schneiderlin@utbm.fr>
2023-04-27 11:31:49 +0200

    Tentative de résolution de problème d'image

--- PageParts/CentralDiv/search.php
+++ PageParts/CentralDiv/search.php
@@ -1,7 +1,5 @@
-<?php $nbrResult = 0;
-while ($row = mysqli_fetch_assoc($searchResults)) {
-   $nbrResult++;
-} ?>
+<?php echo "coucou";//$nbrResult = mysqli_num_rows($searchResults);
+?>
 
 <h1>Résultat de la recherche : <?php $search ?> </h1>
 <h2>Nombres de résultats : <?php echo $nbrResult ?> </h2>
@@ -9,6 +7,7 @@ while ($row = mysqli_fetch_assoc($searchResults)) {
     <?php if($typeSearch == "Topic"){ ?>
         <h2>Topics : </h2>
     <?php } ?>
+
     <?php while ($row = mysqli_fetch_assoc($searchResults)) { ?>
     <li id="one_topic" class="entrees"><a>
             <p class="titre_topic"><?php echo $row["id"] ?></p>
@@ -21,7 +20,7 @@ while ($row = mysqli_fetch_assoc($searchResults)) {
 
 <h2 id="telecharger_regle" class="linkBox"><a href="<?php echo $regles ?>" download="<?php echo 'regles_' . $nomJeu ?>.pdf">Télécharger les règles de <?php echo $nomJeu ?></a></h2>
 <?php if ($connecte) {?>
-    <div id="BoutonCreerMessage" class="linkBox">
+    <div id="BoutonCreerTopicMessageMessage" class="linkBox">
         <a class="police" href="./newMessage.php?topic=<?php echo $idTopic ?>">Créer Message</a>
     </div>
 <?php } ?>
